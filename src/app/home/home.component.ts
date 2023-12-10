eff22e561b62fbccc9ea0e3182173e5de07a4683
BOUHRAOUA Ines DTSI/DTR <QRXL5445@orange.com>
2023-12-10 15:48:02 +0100

    :arrow_up: Added the books display in the home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
 import { AuthService } from "../auth/auth.service";
-
+import { HomeService } from './home.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,10 +10,20 @@ import { AuthService } from "../auth/auth.service";
 export class HomeComponent implements OnInit {
   playerName = '';
   isPlayerNameConfirmed = false;
+  books: any[] = [];
 
-  constructor(private router: Router, private authService: AuthService) { }
+
+  constructor(private router: Router, private authService: AuthService, private homeService: HomeService) { }
 
   ngOnInit(): void {
+    this.homeService.getBooks().subscribe(
+      (data: any[]) => {
+        this.books = data;
+      },
+      error => {
+        console.error('Error fetching books:', error);
+      }
+    );
     //Nous verrons plus tard comment gérer cela avec des observables
     this.authService.isUserConnected();
     this.playerName = this.authService.user?.username || '';
