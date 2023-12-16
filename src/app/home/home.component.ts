2cd2c223479c7624ba4aba4584a82492285666cf
faizaakabli <91140759+faizaakabli@users.noreply.github.com>
2023-12-16 12:01:23 +0100

    add Sort price

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,8 +13,9 @@ import { CartService } from '../shared/services/cart.service';
 export class HomeComponent implements OnInit {
   books: any[] = [];
   filteredBooks: any[] = [];
+  sortDirection: 'asc' | 'desc' = 'asc';
 
-  constructor(private router: Router, private authService: AuthService, private homeService: HomeService , private cartService: CartService) { }
+  constructor(private router: Router, private authService: AuthService, private homeService: HomeService, private cartService: CartService) { }
   ngOnInit(): void {
     this.homeService.getBooks().subscribe(
       (data: any[]) => {
@@ -27,9 +28,27 @@ export class HomeComponent implements OnInit {
     );
   }
 
+  //Barre de recherche
   performSearch(searchTerm: string) {
     this.filteredBooks = this.books.filter(book =>
-      book.titre.toLowerCase().includes(searchTerm.toLowerCase()) 
+      book.titre.toLowerCase().includes(searchTerm.toLowerCase())
     );
+  }
+
+  //Boutton trie 
+  sortBooks() {
+    this.books.sort((a, b) => {
+      const priceA = parseFloat(a.prix.replace('€', '').trim());
+      const priceB = parseFloat(b.prix.replace('€', '').trim());
+
+      if (this.sortDirection === 'asc') {
+        return priceA - priceB;
+      } else {
+        return priceB - priceA;
+      }
+    });
+
+    // Inverser la direction du tri pour le prochain clic
+    this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
   }
 }
